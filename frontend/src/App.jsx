cd9ebc92e76ead1971ccedd128faa9bfb25d3664
nizam <nizam.shejan11@gmail.com>
2025-07-06 14:07:16 +0600

    load data in using axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`/api/jokes`)
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
   return (
     <>
       <h3>Simple React with vite</h3>
+      <h4>All Jokes: {jokes.length}</h4>
+      {jokes.map((joke) => {
+        return (
+          <div key={joke.id}>
+            <h3>Title: {joke.title}</h3>
+            <p>Content: {joke.content}</p>
+          </div>
+        );
+      })}
     </>
   );
 }
